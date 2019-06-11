665f4fd7610bbea23a136d9a031989c9c590ac68
franciscosens <francisco.lucas.sens@gmail.com>
2019-06-11 09:55:12 -0300

    Alterado o tamanho da fonte

--- ExemploBancoDados01/Exemplo00/Form1.Designer.cs
+++ ExemploBancoDados01/Exemplo00/Form1.Designer.cs
@@ -45,10 +45,10 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.Location = new System.Drawing.Point(13, 13);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(86, 31);
+            this.label1.Size = new System.Drawing.Size(93, 33);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nome";
             // 
